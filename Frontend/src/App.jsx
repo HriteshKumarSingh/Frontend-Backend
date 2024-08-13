a7fa9d11acc2e76e7ee50f992d73211c6cf6f2e2
Hritesh Kumar Singh <2003hriteshkumarsingh@gmail.com>
2024-08-13 23:38:16 +0530

    Format frontend code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,9 +11,9 @@ function App() {
         const response = await fetch("/api/users");
         const result = await response.json();
         setUser(result);
-        SetError(false)
+        SetError(false);
       } catch (error) {
-        setUser([])
+        setUser([]);
         SetError(true);
         console.error("An error occours while fetching the data ::", error);
       } finally {
@@ -24,35 +24,50 @@ function App() {
 
   return (
     <div className="bg-slate-900 py-5">
-      {
-        loading == true ? <h1 text-white text-center w-full text-5xl font-semibold py-5>Loading...</h1> : (
+      {loading == true ? (
+        <h1 text-white text-center w-full text-5xl font-semibold py-5>
+          Loading...
+        </h1>
+      ) : (
         <div className="flex flex-col gap-10">
-          {
-            error != false ? <h1 className="text-white text-center w-full text-5xl font-semibold py-5">Unable to fetch data</h1> : <h1 className="text-white text-center w-full text-5xl font-semibold py-5">User Profile</h1>
-          }
+          {error != false ? (
+            <h1 className="text-white text-center w-full text-5xl font-semibold py-5">
+              Unable to fetch data
+            </h1>
+          ) : (
+            <h1 className="text-white text-center w-full text-5xl font-semibold py-5">
+              User Profile
+            </h1>
+          )}
           <div className="flex justify-center items-center flex-wrap gap-10 px-5">
-            {
-              error != true || user.length != 0 ?  (
-              user.map((item) => (
-                <div key={item.id} className="w-72 flex justify-center items-center flex-col gap-6 border-2 border-gray-400 px-6 py-10 rounded-3xl">
-                  <div>
-                    <img src={item.profile_image} alt="profile" className="rounded-full" />
+            {error != true || user.length != 0
+              ? user.map((item) => (
+                  <div
+                    key={item.id}
+                    className="w-72 flex justify-center items-center flex-col gap-6 border-2 border-gray-400 px-6 py-10 rounded-3xl"
+                  >
+                    <div>
+                      <img
+                        src={item.profile_image}
+                        alt="profile"
+                        className="rounded-full"
+                      />
+                    </div>
+                    <div className="text-white text-xl w-full flex flex-col">
+                      <h1>
+                        {item.first_name} {item.last_name}
+                      </h1>
+                      <p>{item.email}</p>
+                      <p>{item.gender}</p>
+                      <p>{item.company}</p>
+                    </div>
                   </div>
-                  <div className="text-white text-xl w-full flex flex-col">
-                    <h1>{item.first_name} {item.last_name}</h1>
-                    <p>{item.email}</p>
-                    <p>{item.gender}</p>
-                    <p>{item.company}</p>
-                  </div>
-                </div>
-              ))
-            ) : null
-            }
+                ))
+              : null}
           </div>
         </div>
-          )
-      }
-      </div>
+      )}
+    </div>
   );
 }
 
